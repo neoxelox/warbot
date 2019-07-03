69f18ab87d5705ce133d2d9d5b12ba1bf6ad6ad8
Neoxelox <alex.rodriguez.navas@gmail.com>
2019-07-04 00:53:47 +0200

    Inverse Dataset

--- src/commands/test.js
+++ src/commands/test.js
@@ -10,15 +10,23 @@ async function test(message, args) {
     let countries = JSON.parse(await readFile("../src/resources/countries.json"));
     let country;
     if(args[1] === undefined) {country = randomProperty(countries);}
-    else country = countries[args[1].toUpperCase()];
-    let cEmbed = new RichEmbed()
-        .setColor(country.color)
-        .setThumbnail(`https://www.countryflags.io/${country.id}/flat/64.png`) // I realized Discord doesn't have preview for .svg images... so this is a workaround
-        .setAuthor(country.name)
-        .setTitle(`**\`${country.id}\`**`)
-        .setDescription(`__Area:__ \`${country.area} km²\` \n __Population:__ \`${country.stats.population} humans\` \n __Attack Points:__ \`${country.stats.attack_points}\` \n __Defend Points:__ \`${country.stats.defend_points}\``)
+    else {
+        country = countries[args[1].toUpperCase()];
+        if(country === undefined) {
+            let inverse = JSON.parse(await readFile("../src/resources/inverse.json"));
+            country = countries[inverse[args[1][0].toUpperCase() + args[1].slice(1)]];
+        }
+    }
+    if(country != undefined) {
+        let cEmbed = new RichEmbed()
+            .setColor(country.color)
+            .setThumbnail(`https://www.countryflags.io/${country.id}/flat/64.png`) // I realized Discord doesn't have preview for .svg images... so this is a workaround
+            .setAuthor(country.name)
+            .setTitle(`**\`${country.id}\`**`)
+            .setDescription(`__Area:__ \`${country.area} km²\` \n __Population:__ \`${country.stats.population} humans\` \n __Attack Points:__ \`${country.stats.attack_points}\` \n __Defend Points:__ \`${country.stats.defend_points}\``)
 
-    message.channel.send(cEmbed);
+        message.channel.send(cEmbed);
+    }
 }
 
 var randomProperty = function (obj) {
